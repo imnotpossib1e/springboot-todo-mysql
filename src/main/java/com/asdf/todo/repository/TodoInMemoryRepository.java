522f9b973511851d4ac4f1eed15b9c549d512594
imnotpossib1e <habit2me@gmail.com>
2026-06-18 23:07:27 +0900

    ✨ feat: Todo 모델 클래스 이동 및 수정

--- src/main/java/com/asdf/todo/repository/TodoInMemoryRepository.java
+++ src/main/java/com/asdf/todo/repository/TodoInMemoryRepository.java
@@ -1,6 +1,6 @@
 package com.asdf.todo.repository;
 
-import com.asdf.todo.model.Todo;
+import com.asdf.todo.entity.Todo;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.stereotype.Repository;
