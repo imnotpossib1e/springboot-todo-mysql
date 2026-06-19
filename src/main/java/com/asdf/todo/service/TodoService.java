3b5161289b2de534eceb74e051e5e80cec1cda7e
imnotpossib1e <habit2me@gmail.com>
2026-06-19 14:48:08 +0900

    ✨ feat: 서비스 레이어 수정

--- src/main/java/com/asdf/todo/service/TodoService.java
+++ src/main/java/com/asdf/todo/service/TodoService.java
@@ -1,11 +1,18 @@
 package com.asdf.todo.service;
 
+import com.asdf.todo.dto.TodoRequestDto;
+import com.asdf.todo.dto.TodoResponseDto;
 import com.asdf.todo.entity.Todo;
 import com.asdf.todo.repository.TodoRepository;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.asdf.todo.util.EntityDtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 // Spring이 비즈니스 로직 계층 빈으로 관리
 // Repository를 주입받아서 DB(인메모리)접근을 위임
@@ -13,38 +20,53 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     private final TodoRepository todoRepository;
 
-    /** 생성자 주입 Spring이 TodoInMemoryRepository를 자동 주입 final로 선언해서 불변성 확보 * */
+    // 생성자 주입
+    // Spring이 TodoInMemoryRepository를 자동 주입 final로 선언해서 불변성 확보
     @Autowired
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    // 전체 Todo 리스트 반환
-    public List<Todo> findAll() {
-        return todoRepository.findAll();
+    // 전체 조회
+    // DB에 있는 모든 Todo를 조최해서 Entity -> ResponseDto로 변환 후 리스트로 반환
+    // 읽기 전용(조회)
+    @Transactional(readOnly = true)
+    public List<TodoResponseDto> findAll() {
+        return todoRepository.findAll().stream() // 전체 조회(findAll()), 리스트 하나씩 처리 가능하게 만들어줌(stream())
+                .map(EntityDtoMapper::toDto) // 각 Entity를 DTO로 변환
+                .collect(Collectors.toList()); // 변환 결과를 List로 만듦
     }
 
     // 단건 조회
-    // 조회 결과가 없을 수 있는 가능성을 명시하게 위해 Optional<T> 타입으로 반환
-    public Optional<Todo> findById(Long id) {
-        return todoRepository.findById(id);
+    // id로 Todo 하나를 조회해서 Entity를 DTO로 변환해서 반환하고, 없으면 null 반환
+    @Transactional(readOnly = true)
+    public TodoResponseDto findById(Long id) {
+        return todoRepository.findById(id).
+                map(EntityDtoMapper::toDto). // 있으면 DTO 반환
+                orElse(null); // 없으면 null
     }
 
     // 저장
-    public Todo save(Todo todo) {
-        return todoRepository.save(todo);
+    // 클라이언트가 보낸 Todo 요청을 데이터 DB에 저장하고 저장된 결과를 응답 DTO로 반환하는 메서드
+    @Transactional
+    public TodoResponseDto save(TodoRequestDto todoRequestDto) {
+        Todo todo = EntityDtoMapper.toEntity(todoRequestDto); // DTO -> Entity
+        Todo savedTodo = todoRepository.save(todo); // DB 저장
+        return EntityDtoMapper.toDto(savedTodo); // Entity -> DTO (DB에 저장된 Entity를 응답 DTO로 변환
     }
 
     // 수정
-    // findByid의 Optional에 맞춰서 id조회, 없으면 예외 발생, 있으면 다음 코드 진행
-    public Todo update(Long id, Todo todo) {
-        todoRepository.findById(id).orElseThrow(() -> new RuntimeException("Todo not fount"));
-
-        todo.setId(id);
-        return todoRepository.save(todo);
+    // 기존 Todo를 수정
+    @Transactional
+    public TodoResponseDto update(Long id, TodoRequestDto todoRequestDto) {
+        Todo todo = EntityDtoMapper.toEntity(todoRequestDto); // DTO -> Entity 변환
+        todo.setId(id); // 아이디 설정(있으면 INSERT, 없으면 UPDATE)
+        Todo updatedTodo = todoRepository.save(todo); // 저장(새 엔티티면 INSERT, 기존 id 있으면 UPDATE)
+        return EntityDtoMapper.toDto(updatedTodo); // 응답용 DTO 반환
     }
 
     // 삭제
+    @Transactional
     public void delete(Long id) {
         todoRepository.deleteById(id);
     }
